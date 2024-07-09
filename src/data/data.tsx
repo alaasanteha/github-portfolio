dddf9afe3f00ab68a827d5f6c62de340cd2a1b3f
Alaa snteha <santehaalaa@gmail.com>
2024-07-09 18:00:00 +0300

    add data

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -73,15 +73,14 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a <strong className="text-stone-100"> Software Engineer</strong>, currently working
+        as a  <strong className="text-stone-100">Front End Developer</strong> with 4+ years of experience building and maintaining high-quality, responsive web applications for different domains, such as e-commerce, education, and health care.
       </p>
-      <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
+      {/* <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
         In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
         plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
         <strong className="text-stone-100">Vancouver Island</strong>.
-      </p>
+      </p> */}
     </>
   ),
   actions: [
@@ -104,16 +103,18 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `Front-End developer with 4+ years of experience building and maintaining high-quality, responsive web applications for different domains, such as e-commerce, education, and health care. 
+
+Proven ability to deliver projects on time and within budget, both independently and collaboratively. Proficient in JavaScript, TypeScript, React Js, Next Js, Laravel, PHP. Hands-on experience with Agile development methodologies and a proven ability to translate complex business requirements into user-friendly digital interfaces.
+
+ I have the motivation to learn new technologies and skills, and to self-develop as a Software Engineer. I always want to be part of a hardworking, creative, and understanding team that shares the same vision and goals.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    { label: 'Location', text: 'Turkey, Istanbul', Icon: MapIcon },
+    { label: 'Age', text: '32', Icon: CalendarIcon },
+    { label: 'Nationality', text: 'Syrian', Icon: FlagIcon },
+    { label: 'Interests', text: 'Reading, Zomba, Yoga', Icon: SparklesIcon },
+    { label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon },
+    { label: 'Employment', text: 'Alemi, IT Services and IT Consulting.', Icon: BuildingOffice2Icon },
   ],
 };
 
@@ -125,16 +126,20 @@ export const skills: SkillGroup[] = [
     name: 'Spoken languages',
     skills: [
       {
-        name: 'English',
+        name: 'Arabic',
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'English',
+        level: 8,
       },
       {
-        name: 'Spanish',
-        level: 3,
+        name: 'French',
+        level: 2,
+      },
+      {
+        name: 'Turkish',
+        level: 6,
       },
     ],
   },
@@ -143,15 +148,19 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'React',
-        level: 9,
+        level: 8,
+      },
+      {
+        name: 'Next.js',
+        level: 8,
       },
       {
         name: 'Typescript',
-        level: 7,
+        level: 6,
       },
       {
-        name: 'GraphQL',
-        level: 6,
+        name: 'Javascript',
+        level: 8,
       },
     ],
   },
@@ -159,33 +168,12 @@ export const skills: SkillGroup[] = [
     name: 'Backend development',
     skills: [
       {
-        name: 'Node.js',
-        level: 8,
-      },
-      {
-        name: 'Rust',
-        level: 5,
-      },
-      {
-        name: 'Golang',
-        level: 4,
-      },
-    ],
-  },
-  {
-    name: 'Mobile development',
-    skills: [
-      {
-        name: 'React Native',
-        level: 9,
-      },
-      {
-        name: 'Flutter',
-        level: 4,
-      },
-      {
-        name: 'Swift',
+        name: 'Python',
         level: 3,
+      },
+      {
+        name: 'Node.js',
+        level: 2,
       },
     ],
   },
@@ -196,70 +184,40 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'AlifBee Web App',
+    description: 'It is a web application for teaching the Arabic language to non-native speakers with professional and enjoyable ways which has shown great success around the world .',
+    url: 'https://app.alifbee.com',
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage2,
+    title: 'AlifBee Landing Page',
+    description: 'I focused on this project to have perfect performance and SEO-friendly',
+    url: 'https://www.alifbee.com',
+    image: porfolioImage1,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'AlifBee Academy',
+    description: 'Technologies: React • RESTful API • Responsive Design • React Hooks • Bootstrap 5',
+    url: 'https://academy.alifbee.com/',
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'AlifBee Kids Landing page',
+    description: 'I focused on this project to have perfect performance and SEO-friendly.',
+    url: 'https://alifbeekids.com/',
     image: porfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'AlifBee LMS',
+    description: 'Building web applications to manage the content of the entire curriculums for more than one language and educational institution using Laravel framework which follows the Model-View-Controller (MVC).',
+    url: 'https://lms.alifbee.com/',
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: 'Premium Surplus',
+    description: 'I worked on this project as a freelancer. Technologies: Next.js • Material-UI • HTML/CSS • RESTful API • MVC',
+    url: 'https://psurplus.com/',
     image: porfolioImage6,
-  },
-  {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage7,
-  },
-  {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage8,
-  },
-  {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage9,
-  },
-  {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage10,
-  },
-  {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
-    image: porfolioImage11,
   },
 ];
 
@@ -268,39 +226,60 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'February 2017',
+    location: 'Damascus University',
+    title: 'Bachelor degree in Computer And Programming Engineering',
+    content: <p></p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'February 2017',
+    location: 'Coursera.org',
+    title: 'Front-End Web UI Frameworks and Tools: Bootstrap 4 - The Hong Kong University of Science and Technology',
+    content: <p></p>,
+  },
+  {
+    date: 'June 2024',
+    location: 'Coursera.org',
+    title: 'Programming in Python',
+    content: <p></p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'Dec 2019 - Dec 2023',
+    location: 'AlifBee Company',
+    title: 'Front-end Engineer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        I worked with the team on many web applications and there were many challenges and the team had a
+        very competitive spirit . One of the most important projects we lunched was a project to teach the Arabic
+        language to non-native speakers which has shown great success around the world using different web
+        technologies and platform such as React js, Next js, JavaScript, HTML, CSS, jQuery, Java, bootstrap and
+        styled component.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'Jan 2018 – Apr 2018 ',
+    location: 'Al Manara Soft',
+    title: 'Junior Software Programmer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Building web applications that support different industries such as e-commerce and personal websites
+        using different web technologies and platform such as .NET framework , JavaScript, HTML, CSS and
+        Angular 6.
+      </p>
+    ),
+  },
+  {
+    date: 'Feb 2019 – Dec 2022 ',
+    location: 'Freelance',
+    title: 'Web Developer',
+    content: (
+      <p>
+        Building and designing e-commerce websites using React js, Next js, JavaScript, HTML, HTML5, CSS, CSS3,
+        jQuery, Java, bootstrap, MUI Material and styled component.
       </p>
     ),
   },
@@ -313,19 +292,24 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+      name: 'Abdalhady Alsayad',
+      text: "I've had the pleasure of working with Alaa for the past three years, and during this time, she has proven to be a remarkable and talented frontend developer. Alaa consistently demonstrates a strong commitment to her work, coupled with a keen eagerness to learn and master new technologies. Her skills in software architecture and design patterns are particularly impressive, making her a valuable asset to any team.",
+      image: '',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+      name: 'Khaled Alsayad',
+      text: 'I had the pleasure of working with Alaa on Alifbee. She is a highly skilled and talented engineer who consistently impressed me with her technical abilities and work ethic.',
+      image: '',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+      name: 'Mumin Osman',
+      text: 'Alaa is an exceptional professional who consistently delivers high-quality results. Their expertise and dedication make them a valuable asset to any team. I highly recommend her for her outstanding skills and contributions.',
+      image: '',
+    },
+    {
+      name: 'Mohamad Bitar',
+      text: "I highly recommend Alaa Santeha as a talented full stack developer. I had the pleasure of working closely with Alaa for the past three years at Alemi Media Company. Alaa possesses a wealth of technical expertise in various programming languages including JavaScript, PHP, and SQL. She is skilled at using a variety of tools and frameworks such as Git, ReactJS, VueJS, and Laravel. One project I worked on with Alaa was the development of a new e-learning platform called AlifBee. Alaa was responsible for developing the platform's frontend, which included implementing a lot of UI/UX features. She worked tirelessly to ensure that the platform was scalable, secure, and user-friendly. Her contributions were instrumental in the success of the project. Overall, I highly recommend Alaa as a skilled full stack developer. Her technical expertise, strong work ethic, and excellent interpersonal skills make her a valuable asset to any team.",
+      image: '',
     },
   ],
 };
@@ -336,27 +320,27 @@ export const testimonial: TestimonialSection = {
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: '',
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
+      text: 'santehaalaa@gmail.com',
       href: 'mailto:reachout@timbaker.me',
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: 'Istanbul, Turkey',
+      href: 'https://www.google.ca/maps/place/%C4%B0stanbul/@41.0054632,28.8473759,11z',
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: '@alaa.santeha',
+      href: 'https://www.instagram.com/alaa.santeha/',
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: 'alaasanteha',
+      href: 'https://github.com/alaasanteha',
     },
   ],
 };
@@ -365,9 +349,9 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx' },
+  { label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker' },
+  { label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/' },
+  { label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/' },
+  { label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx' },
 ];
